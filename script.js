cde3ab3dfd0042f99aca491d564648c7dc06e990
Abdulaziz Ali <zizogmd@gmail.com>
2022-02-27 17:09:27 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var amount = document.getElementById("amount").value;
+    var myRate = document.getElementById("myRate").value;
+    var years = document.getElementById("years").value;
+    var resultDeposit = document.getElementById("resultDeposit");
+    var resultRate = document.getElementById("resultRate");
+    var resultAmount = document.getElementById("resultAmount");
+    var resultYear = document.getElementById("resultYear");
+
+    var newAmount = parseFloat(((amount * (myRate / 100)) * years))
+
+    var year = new Date().getFullYear(); 
+
+    year = parseInt(year) + parseInt(years);
+
+    resultDeposit.innerHTML = "If you deposit " + amount + ","; 
+    resultRate.innerHTML = "at an interest rate of " + myRate + "%,"; 
+    resultAmount.innerHTML = "You will receive an amount of " + newAmount + ","; 
+    resultYear.innerHTML = "in the year " + year ; 
+}
+
+function updateRate() {
+    var myRate = document.getElementById("myRate").value;
+    document.getElementById("rateShow").innerHTML = myRate + "%";
 }
         
